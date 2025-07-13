c84fd90aea7765a142776552fb2004a3860fb6b6
Gakkou1213 <yamamoto20031213@gmail.com>
2025-07-13 18:57:33 +0900

    add yesorno

--- routes/cat.js
+++ routes/cat.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const request = require('request');
-
 router.get('/', async (req, res) => {
-request('https://api.thecatapi.com/v1/images/search', function (error, response,
+request('https://yesno.wtf/api', function (error, response,
 body) {
 if (!error && response.statusCode == 200) {
 const data = JSON.parse(body);
